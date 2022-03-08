d17a6b2cfeaf687e034559899f0023533a649478
Sebastian Scheffler <seb.scheffler@gmx.de>
2022-03-08 01:40:58 +0100

    Configured root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'store/index'
+  # controller#action = 'store#index'
+  # as: tells Rails to create store_index_path and store_index_url accessor methods.
+  root 'store#index', as: 'store_index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

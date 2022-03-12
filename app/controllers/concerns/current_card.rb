0c14ac2037a8abfb354856301cfc3bd7868105be
Sebastian Scheffler <seb.scheffler@gmx.de>
2022-03-12 15:35:53 +0100

    Connected cart with session

---
+++ app/controllers/concerns/current_card.rb
@@ -0,0 +1,21 @@
+module CurrentCart
+
+  # Note that we place the set_cart() method in a CurrentCart module and place that module in a new
+  # file in the app/controllers/concerns directory. This treatment allows us to *share* common code
+  # (even as little as a single method!) among controllers.
+
+  # We mark the method as private, which prevents Rails from ever making it available as an action
+  # on the controller.
+  private
+
+  # The set_cart() method starts by getting the :cart_id from the session object and then attempts to
+  # find a cart corresponding to this ID. If such a cart record isn’t found (which will happen if the
+  # ID is nil or invalid for any reason), this method will proceed to create a new Cart and then store
+  # the ID of the created cart into the session.
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create session[:cart_id] = @cart.id
+  end
+end
